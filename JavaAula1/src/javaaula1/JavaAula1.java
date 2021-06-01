1550e4cc3c6b95462810d066ccb9bfb1207589c1
Antonio Augusto Gontijo <antonioasgontijo@outlook.com>
2021-06-01 14:35:24 -0300

    Update JavaAula1.java
    Adição de recursos para mostrar a resolução da tela e a data da maquina do usuário.

--- JavaAula1/src/javaaula1/JavaAula1.java
+++ JavaAula1/src/javaaula1/JavaAula1.java
@@ -1,6 +1,11 @@
 
 package javaaula1;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class JavaAula1 {
 
@@ -8,6 +13,29 @@ public class JavaAula1 {
        
         System.out.println("Olá Mundo!");
         System.out.println("Hello");
+        
+        
+        Date relogio = new Date();
+        
+        System.out.println("A hora do sistema é:");
+        System.out.println(relogio.toString());
+        
+        
+        System.getProperties();
+        System.out.println(System.getProperty("user.language"));
+        
+        Toolkit t = Toolkit.getDefaultToolkit();
+        Dimension d = t.getScreenSize();
+        System.out.println("A resolução da tela é: " +d.width + "x" +d.height);
+        
+        
+        Locale idioma = Locale.getDefault();
+        System.out.println("O idioma do seu sistema é " + idioma.getDisplayLanguage());
+        //System.out.println(idioma.getDisplayLanguage());
+        
+        
+        
+        
     }
     
 }
