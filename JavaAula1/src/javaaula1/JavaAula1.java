6856977b080794f2ea78ef14baaf7705220641a3
Antonio Augusto Gontijo <antonioasgontijo@outlook.com>
2021-06-01 11:42:12 -0300

    OláMundo
    Código inicial

---
+++ JavaAula1/src/javaaula1/JavaAula1.java
@@ -0,0 +1,13 @@
+
+package javaaula1;
+
+
+public class JavaAula1 {
+
+    public static void main(String[] args) {
+       
+        System.out.println("Olá Mundo!");
+        System.out.println("Hello");
+    }
+    
+}
